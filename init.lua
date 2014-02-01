6aaa0f9b8b799173fa7c1029f26c9416d7bce9e7
Alejandro Baez <Alejan.Baez@gmail.com>
2014-02-01 17:37:17 -0500

    Code done. Now debug
    Most of the code is done. The rest is to actually debug this sucker.
    Next commit should be fixed of errors.

--- init.lua
+++ init.lua
@@ -1,18 +1,33 @@
 local M = {}
+--setmetatable(M, meta)
+local meta = {}
 
 local ul = require("ul")
 
---DEFAULT_URL = "https://code.undonestar.tk"
 DEFAULT_URL = "http://localhost:7777"
 
-function M:init(url)
-  self.url = (url or DEFAULT_URL) .. "/documents"
+
+local chunk
+
+function meta.__call()
+
+  if #buffer:get_sel_text() == 0 then
+    chunk = {}
+    for line in io.open(buffer.filename):lines("*L") do
+      chunk[#chunk + 1] = line
+    end
+
+    chunk = table.concat(chunk)
+  else
+    chunk = buffer:get_sel_text()
+  end
+
+  paste = ul.post(DEFAULT_URL, chunk)
+
+  final_url = DEFAULT_URL .. "/" .. paste["key"]
+  ui.statusbar_text(string.format("%s added to the clipboard"), final_url)
+  ui.clipboard_text = final_url
 end
 
-function M:ul_path(path)
-
-end
-
-function ul_raw(data)
-
-end
+setmetatable(M, meta)
+return M
--- ul.lua
+++ ul.lua
@@ -1,29 +1,19 @@
 
-
 local http = require("socket.http")
 local ltn12 = require("ltn12")
 
-f = io.open("star_jack.sh", "r")
 
-  local t = {}
-  for line in f:lines("*L") do
-    t[#t + 1] = line
-  end
-f = table.concat(t)
---print(f:seek("end"))
-
-
-local function post(file, url)
+local function post(chunk, url)
   -- table which will hold the response of the server
   local response = {}
   assert(http.request{
-    url = url or haste_default,
+    url = url .. "/documents",
     redirect = true,
     headers = {
-      ["content-length"] = #file
+      ["content-length"] = #chunk
     },
     method = "POST",
-    source = ltn12.source.string(file),
+    source = ltn12.source.string(chunk),
     sink = ltn12.sink.table(response)
   })
 
